4de595126c098501db1dd63e8f0780ecb90d05d9
William Chang <wfchang@usc.edu>
2020-07-03 20:29:19 -0700

    added computer keyboard support

--- js/interactive-piano.js
+++ js/interactive-piano.js
@@ -1,5 +1,20 @@
 const NUM_OCTAVES = 3;
 const STARTING_OCTAVE = 3; // keyboard starts on C3
+const KEY_PATTERN = [0,1,0,1,0,0,1,0,1,0,1,0];
+
+// computer key to piano key mapping
+const FULL_KEYMAP = ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH',
+                     'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6',
+                     'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight',
+                     'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight'];
+const FULL_KEYMAP_CHAR = ['SL', 'Z', 'X', 'C', 'V', 'B', 'A', 'S', 'D', 'F', 'G', 'H',
+                          'Q', 'W', 'E', 'R', 'T', 'Y', '1', '2', '3', '4', '5', '6',
+                          '7', '8', '9', '0', '-', '=', 'U', 'I', 'O', 'P', '[', ']',
+                          'J', 'K', 'L', ';', '"', 'EN', 'N', 'M', ',', '.', '/', 'SR'];
+
+// reduced keymapping for 3 octaves
+const KEYMAP = FULL_KEYMAP.slice(6,-5);
+const KEYMAP_CHAR = FULL_KEYMAP_CHAR.slice(6, -5);
 
 var keysPressed = [];
 var intervals = [];
@@ -7,39 +22,68 @@ var intervals = [];
 function renderKeyboard() {
     var keyboardHTML = "<ul class='keyboard'>\n";
 
-    // fill in keys
+    // each iteration is an octave
     for (var i = 0; i < NUM_OCTAVES; i++) {
-        keyboardHTML += "\
-        <li class='white-key'></li>\n\
-        <li class='black-key'></li>\n\
-        <li class='white-key'></li>\n\
-        <li class='black-key'></li>\n\
-        <li class='white-key'></li>\n\
-        <li class='white-key'></li>\n\
-        <li class='black-key'></li>\n\
-        <li class='white-key'></li>\n\
-        <li class='black-key'></li>\n\
-        <li class='white-key'></li>\n\
-        <li class='black-key'></li>\n\
-        <li class='white-key'></li>\n"
+        // each iteration is one note
+        for (var j = 0; j < 12; j++) {
+            const color = KEY_PATTERN[j] ? 'black' : 'white';
+            keyboardHTML += "<li class='" + color + "-key'><p>" + KEYMAP_CHAR[i*12 + j] + "</p></li>\n";
+        }
     }
 
-    keyboardHTML += "<li class='white-key'></li>\n</ul>\n";
+    keyboardHTML += "<li class='white-key'><p>" + KEYMAP_CHAR[NUM_OCTAVES*12] + "</p></li>\n</ul>\n";
 
     return keyboardHTML;
 }
 
-function processPianoKeypress() {
+function processPianoClick() {
     const keyIndex = $(this).index(); // get the pressed keyboard key
+    if (keyIndex < 0 || keyIndex > NUM_OCTAVES * 12) return;
+    playKeyAudio(keyIndex); // play audio
 
-    // play audio for the key
-    var audio = new Audio('static/key_sounds/' + getPianoKeyID(keyIndex) + '.m4a');
+    addNote(keyIndex); // update notes history
+}
+
+function processComputerKeydown(e) {
+    const keyIndex = KEYMAP.indexOf(e.code);
+    if (keyIndex < 0 || keyIndex > NUM_OCTAVES * 12) return;
+    playKeyAudio(keyIndex); // play audio
+
+    addNote(keyIndex); // update notes history
+
+    // change style of piano key
+    const pianoKey = $('.keyboard').find('li').eq(keyIndex);
+    if (pianoKey.hasClass('white-key')) pianoKey.addClass('white-key-active');
+    else pianoKey.addClass('black-key-active');
+}
+
+function processComputerKeyup(e) {
+    const keyIndex = KEYMAP.indexOf(e.code);
+    if (keyIndex < 0 || keyIndex > NUM_OCTAVES * 12) return;
+
+    // change style of piano key back to default
+    const pianoKey = $('.keyboard').find('li').eq(keyIndex);
+    if (pianoKey.hasClass('white-key')) pianoKey.removeClass('white-key-active');
+    else pianoKey.removeClass('black-key-active');
+}
+
+// Helper function to play the audio clip for the pressed key
+function playKeyAudio(keyIndex) {
+    // Get key ID (D4, A5, Gb3, etc.)
+    const allKeys = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B'];
+    const keyID = allKeys[keyIndex % 12] + Math.floor(STARTING_OCTAVE+keyIndex/12);
+
+    // Play audio for the key
+    var audio = new Audio('static/key_sounds/' + keyID + '.m4a');
     audio.play();
+}
 
+// Helper function to update arrays holding note history data
+function addNote(keyIndex) {
     // update interval info if more than one key has been pressed
-    if(keysPressed.length > 0) {
-        const newInterval = keyIndex - keysPressed[keysPressed.length-1];
-        
+    if (keysPressed.length > 0) {
+        const newInterval = keyIndex - keysPressed[keysPressed.length - 1];
+
         $('#intervals-display').append(newInterval + ', ');
         intervals.push(newInterval);
     }
@@ -47,11 +91,4 @@ function processPianoKeypress() {
     // either way, update notes info
     $('#notes-display').append(keyIndex + ', ');
     keysPressed.push(keyIndex);
-}
-
-// D4, A5, etc.
-function getPianoKeyID(keyIndex) {
-    const allKeys = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B'];
-    console.log(allKeys[keyIndex % 12] + Math.floor(STARTING_OCTAVE + keyIndex / 12));
-    return allKeys[keyIndex % 12] + Math.floor(STARTING_OCTAVE+keyIndex/12);
 }
